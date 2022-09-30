bafdae975a506c77beb3d20e8536a4334b1e420d
Muhammad Hassam Baig <63207632+HassamMughal3211@users.noreply.github.com>
2022-09-30 22:25:24 +0500

    upated

---
+++ src/components/SecondNavbar/SecondNavbar.jsx
@@ -0,0 +1,23 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+import "./SecondNavbar.css"
+
+const SecondNavbar = () => {
+  return (
+    <Grid container className="secondNavMain flex">
+        <Grid md={12} item className='flex'>
+            <button className="secondNavButtonShadow " >2D Animation</button>
+            <button className="secondNavButtonShadow" >3D Animation</button>
+            <button className="secondNavButton" >Whiteboard</button>
+            <button className="secondNavButtonShadow " >2D Animation</button>
+            <button className="secondNavButtonShadow" >3D Animation</button>
+            <button className="secondNavButton" >Whiteboard</button>
+        </Grid>
+        <Grid item container className='gridPage' xs={12} >
+
+        </Grid>
+    </Grid>
+  )
+}
+
+export default SecondNavbar
---
+++ src/pages/Illustration/Illustration.jsx
@@ -0,0 +1,143 @@
+import { AspectRatio } from "@mui/joy";
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import LandingNavbar from "../../components/LandingNavbar/LandingNavbar";
+import MiddleNav from "../../components/MiddleNav/MiddleNav";
+import SecondNavbar from "../../components/SecondNavbar/SecondNavbar";
+import "./Illustration.css";
+
+const Illustration = () => {
+  return (
+    <>
+      {/* <LandingNavbar /> */}
+      {/* <SecondNavbar /> */}
+      <Grid Container className="content">
+        <Grid className="title flex">
+          <Typography variant="h3"> Logo </Typography>
+        </Grid>
+        <Grid className="row1" xs={12}>
+          <Grid className="column1" md={3} xs={12}>
+            <Grid className="item1 flex" xs={12}>
+              <AspectRatio
+                ratio="1"
+                sx={{ minWidth: 100, borderRadius: "sm", overflow: "auto" }}
+              >
+                <img
+                  style={{ background: "black" }}
+                  height="100"
+                  width="100"
+                  src={
+                    "https://play-lh.googleusercontent.com/gCyyBvrSQ1jiiZxgBu4Mm2wgH7F5rBRXE2H-Uji9wnRHcnDwxx6kMp3kEy8TWlKvsg"
+                  }
+                  // srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"item.titl"}
+                />
+              </AspectRatio>
+            </Grid>
+            <Grid className="item1 flex" xs={12}>
+              <AspectRatio
+                ratio="1"
+                sx={{ minWidth: 100, borderRadius: "sm", overflow: "auto" }}
+              >
+                <img
+                  style={{ background: "black" }}
+                  height="100"
+                  width="100"
+                  src={
+                    "https://play-lh.googleusercontent.com/gCyyBvrSQ1jiiZxgBu4Mm2wgH7F5rBRXE2H-Uji9wnRHcnDwxx6kMp3kEy8TWlKvsg"
+                  }
+                  // srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"item.titl"}
+                />
+              </AspectRatio>
+            </Grid>{" "}
+          </Grid>
+          <Grid className="column2" md={6} xs={12}>
+            <Grid className="item2" xs={12}>
+              <AspectRatio
+                ratio="1"
+                sx={{ minWidth: 200, borderRadius: "sm", overflow: "auto" }}
+              >
+                <img
+                  style={{ background: "black" }}
+                  height="100"
+                  width="100"
+                  src={
+                    "https://play-lh.googleusercontent.com/gCyyBvrSQ1jiiZxgBu4Mm2wgH7F5rBRXE2H-Uji9wnRHcnDwxx6kMp3kEy8TWlKvsg"
+                  }
+                  // srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"item.titl"}
+                />
+              </AspectRatio>
+            </Grid>
+          </Grid>
+          <Grid className="column1" md={3} xs={12}>
+            <Grid className="item1 flex" xs={12}>
+              <AspectRatio
+                ratio="1"
+                sx={{ minWidth: 100, borderRadius: "sm", overflow: "auto" }}
+              >
+                <img
+                  style={{ background: "black" }}
+                  height="100"
+                  width="100"
+                  src={
+                    "https://play-lh.googleusercontent.com/gCyyBvrSQ1jiiZxgBu4Mm2wgH7F5rBRXE2H-Uji9wnRHcnDwxx6kMp3kEy8TWlKvsg"
+                  }
+                  // srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"item.titl"}
+                />
+              </AspectRatio>
+            </Grid>{" "}
+            <Grid className="item1 flex" xs={12}>
+              <AspectRatio
+                ratio="1"
+                sx={{ minWidth: 100, borderRadius: "sm", overflow: "auto" }}
+              >
+                <img
+                  style={{ background: "black" }}
+                  height="100"
+                  width="100"
+                  src={
+                    "https://play-lh.googleusercontent.com/gCyyBvrSQ1jiiZxgBu4Mm2wgH7F5rBRXE2H-Uji9wnRHcnDwxx6kMp3kEy8TWlKvsg"
+                  }
+                  // srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"item.titl"}
+                />
+              </AspectRatio>
+            </Grid>{" "}
+          </Grid>
+        </Grid>
+
+        <Grid className="row2" xs={12}>
+          <Grid className="item2" md={3} xs={12}></Grid>
+          <Grid className="item2" md={3} xs={12}></Grid>
+          <Grid className="item2" md={3} xs={12}></Grid>
+          <Grid className="item2" md={3} xs={12}></Grid>
+        </Grid>
+        <Grid className="row1" xs={12}>
+          <Grid className="column1" xs={3}>
+            <Grid className="item1" xs={12}></Grid>
+            <Grid className="item1" xs={12}></Grid>
+          </Grid>
+          <Grid className="column2" xs={6}>
+            <Grid className="item2" xs={12}></Grid>
+          </Grid>
+          <Grid className="column1" xs={3}>
+            <Grid className="item1" xs={12}></Grid>
+            <Grid className="item1" xs={12}></Grid>
+          </Grid>
+        </Grid>
+
+        <Grid className="row2" xs={12}>
+          <Grid className="item2" xs={3}></Grid>
+          <Grid className="item2" xs={3}></Grid>
+          <Grid className="item2" xs={3}></Grid>
+          <Grid className="item2" xs={3}></Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Illustration;
