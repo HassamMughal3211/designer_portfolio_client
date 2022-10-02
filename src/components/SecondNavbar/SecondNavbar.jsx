339e985bcba40ab0cd88f4f1ef2b4639093c4324
Zaid Ahmed <41258175+SZaidAhmed@users.noreply.github.com>
2022-10-02 20:48:51 +0500

    dropdown

--- src/components/SecondNavbar/SecondNavbar.jsx
+++ src/components/SecondNavbar/SecondNavbar.jsx
@@ -1,31 +1,96 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import React from "react";
 import "./SecondNavbar.css";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useTheme } from '@mui/material/styles';
 
-const SecondNavbar = () => {
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Logo', 'T-shirt', 'Banner', 'Business Card', 'Brand identity', 'Social media post'
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+const SecondNavbar = ({ setSubCategory }) => {
+  const [personName, setPersonName] = React.useState('Logo');
+  const theme = useTheme();
+  const handleChange = (e) => {
+    console.log(e.target.value[0])
+    setPersonName(e.target.value )
+  };
+
+
+
+
   return (
     <Grid container className="secondNav flex">
       {/* <span className="space" style={{height:"10vh", width:"100v", backghroundColor:"black"}} >     </span> */}
-      <Grid iem md={12} sx={{p:0.5, mt:"10vh" }}  item className="flex">
-        <button item className="secondNavButtonShadow ">
+      <Box display={{ xs: "none", md: "block" }} className="flex">
+        <button item onClick={setSubCategory("logo")} className="secondNavButtonShadow ">
           LOGO
         </button>
-        <button item className="secondNavButtonShadow">
+        <button item onClick={setSubCategory("t-shirt")} className="secondNavButtonShadow">
           T-shirt
         </button>
-        <button item className="secondNavButton">
-          Banners
+        <button item onClick={setSubCategory("banner")} className="secondNavButton">
+          Banner
         </button>
-        <button item className="secondNavButtonShadow ">
+        <button item onClick={setSubCategory("businesscard")} className="secondNavButtonShadow ">
           Business Card
         </button>
-        <button item className="secondNavButtonShadow">
+        <button item onClick={setSubCategory("brandidentity")} className="secondNavButtonShadow">
           Brand identity
         </button>
-        <button item className="secondNavButton">
+        <button item onClick={setSubCategory("socailmediapost")} className="secondNavButton">
           Social media post
         </button>
-      </Grid>
+      </Box>
+      <Box display={{ xs: "block", md: "none" }} className="flex" >
+        <div>
+          <FormControl sx={{ m: 1, width: 300 }}>
+            <InputLabel id="demo-multiple-name-label">Sub category</InputLabel>
+            <Select
+              labelId="demo-multiple-name-label"
+              id="demo-multiple-name"
+              value={personName}
+              onChange={handleChange}
+              input={<OutlinedInput label="Sub category" />}
+              MenuProps={MenuProps}
+            >
+              {names.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, personName, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </Box>
     </Grid>
   );
 };
