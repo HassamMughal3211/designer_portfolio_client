0c8e5b3f92e9c00aedb39577f70de935f699b24d
Muhammad Hassam Baig <63207632+HassamMughal3211@users.noreply.github.com>
2022-10-02 18:06:02 +0500

    updadted

--- src/components/SecondNavbar/SecondNavbar.jsx
+++ src/components/SecondNavbar/SecondNavbar.jsx
@@ -1,23 +1,33 @@
-import { Grid } from '@mui/material';
-import React from 'react';
-import "./SecondNavbar.css"
+import { Grid } from "@mui/material";
+import React from "react";
+import "./SecondNavbar.css";
 
 const SecondNavbar = () => {
   return (
     <Grid container className="secondNav flex">
-        <Grid md={12} item className='flex'>
-            <button className="secondNavButtonShadow " >2D Animation</button>
-            <button className="secondNavButtonShadow" >3D Animation</button>
-            <button className="secondNavButton" >Whiteboard</button>
-            <button className="secondNavButtonShadow " >2D Animation</button>
-            <button className="secondNavButtonShadow" >3D Animation</button>
-            <button className="secondNavButton" >Whiteboard</button>
-        </Grid>
-        <Grid item container className='gridPage' xs={12} >
-
-        </Grid>
+      <Grid iem md={12} item className="flex">
+        <button item className="secondNavButtonShadow ">
+          2D Animation
+        </button>
+        <button item className="secondNavButtonShadow">
+          3D Animation
+        </button>
+        <button item className="secondNavButton">
+          Whiteboard
+        </button>
+        <button item className="secondNavButtonShadow ">
+          2D Animation
+        </button>
+        <button item className="secondNavButtonShadow">
+          3D Animation
+        </button>
+        <button item className="secondNavButton">
+          Whiteboard
+        </button>
+      </Grid>
+      <Grid item container className="gridPage" xs={12}></Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default SecondNavbar
+export default SecondNavbar;
--- src/pages/constants/constants.js
+++ src/pages/constants/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:8000"
+// export const BASE_URL = "http://localhost:8000"
+// export const BASE_URL = "http://192.168.3.108:8000"
+export const BASE_URL = "https://mozack-portfolio.herokuapp.com"
