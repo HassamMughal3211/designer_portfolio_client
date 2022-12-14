6f5506a59365be4308526ae37d5d97a88bce6390
Muhammad Hassam Baig <63207632+HassamMughal3211@users.noreply.github.com>
2022-12-14 22:16:04 +0500

    updated

--- src/components/MiddleNav/MiddleNav.jsx
+++ src/components/MiddleNav/MiddleNav.jsx
@@ -1,8 +1,8 @@
-import { Grid } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { BASE_URL } from '../../pages/constants/constants';
-import "./MiddleNav.css"
-import CircularProgress from '@mui/material/CircularProgress';
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../../pages/constants/constants";
+import "./MiddleNav.css";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const MiddleNav = () => {
   const [allFiles, setAllFiles] = useState([]);
@@ -11,51 +11,114 @@ const MiddleNav = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`${BASE_URL}/api/v1/project/filterProjects?category=${category}&subCategory=${subCategory}`).then((response) => response.json()).then((data) => setAllFiles(data.data))
+    fetch(
+      `${BASE_URL}/api/v1/project/filterProjects?category=${category}&subCategory=${subCategory}`
+    )
+      .then((response) => response.json())
+      .then((data) => setAllFiles(data.data));
     setLoading(false);
   }, []);
 
   const categoryButtonClicked = async (param) => {
-    console.log("params", param)
-    setLoading(true)
+    console.log("params", param);
+    setLoading(true);
     setSubCategory(param);
-    await fetch(`${BASE_URL}/api/v1/project/filterProjects?category=${category}&subCategory=${param}`).then((response) => response.json()).then((data) => setAllFiles(data.data))
-    setLoading(false)
-  }
+    await fetch(
+      `${BASE_URL}/api/v1/project/filterProjects?category=${category}&subCategory=${param}`
+    )
+      .then((response) => response.json())
+      .then((data) => setAllFiles(data.data));
+    setLoading(false);
+  };
 
   return (
     <Grid container className="middleNavMain flex">
-      <Grid xs={12} item style={{ height: '100%', background: 'white', zIndex: '1' }} className='flex'>
-
-        <Grid md={6} xs={12} item style={{ height: '100%', background: 'white' }} className='flex'>
+      <Grid
+        xs={12}
+        item
+        style={{ height: "100%", background: "white", zIndex: "1" }}
+        className="flex"
+      >
+        <Grid
+          md={6}
+          xs={12}
+          item
+          style={{ height: "100%", background: "white" }}
+          className="flex"
+        >
           <button
-            className={subCategory === '2danimation' ? "middleNavButton" : "middleNavButtonShadow"}
-            onClick={() => categoryButtonClicked('2danimation')} >2D Animation</button>
+            className={
+              subCategory === "2danimation"
+                ? "middleNavButton"
+                : "middleNavButtonShadow"
+            }
+            onClick={() => categoryButtonClicked("2danimation")}
+          >
+            2D Animation
+          </button>
           <button
-            className={subCategory === '3danimation' ? "middleNavButton" : "middleNavButtonShadow"}
-            onClick={() => categoryButtonClicked('3danimation')} >3D Animation</button>
+            className={
+              subCategory === "3danimation"
+                ? "middleNavButton"
+                : "middleNavButtonShadow"
+            }
+            onClick={() => categoryButtonClicked("3danimation")}
+          >
+            3D Animation
+          </button>
           <button
-            className={subCategory === 'whiteboard' ? "middleNavButton" : "middleNavButtonShadow"}
-            onClick={() => categoryButtonClicked('whiteboard')} >Whiteboard</button>
+            className={
+              subCategory === "whiteboard"
+                ? "middleNavButton"
+                : "middleNavButtonShadow"
+            }
+            onClick={() => categoryButtonClicked("whiteboard")}
+          >
+            Whiteboard
+          </button>
         </Grid>
       </Grid>
-      <Grid item container className='gridPage flex' xs={12} style={{ zIndex: '2' }} >
-        <Grid item container md={12} xs={11} style={{ height: "100%", justifyContent: "space-evenly", marginBottom: "20px" }} >
-          {
-            !loading ?
-              allFiles.map((item, idx) => (
-                <Grid item sm={3.8} xs={12} className="gridPageBox">
-                  <video src={`${BASE_URL}${item.fileData[0]}`} alt={idx} id={idx} onMouseOver={event => event.target.play()}
-                    onMouseOut={event => event.target.pause()} controls={false} autoPlay={false} loop="true" muted height="100%" width="100%" />
-                </Grid>
-              ))
-              :
-              <CircularProgress className='animationLoader' disableShrink />
-          }
+      <Grid
+        item
+        container
+        className="gridPage flex"
+        xs={12}
+        style={{ zIndex: "2" }}
+      >
+        <Grid
+          item
+          container
+          md={12}
+          xs={11}
+          style={{
+            height: "100%",
+            justifyContent: "space-evenly",
+            marginBottom: "20px",
+          }}
+        >
+          {!loading ? (
+            allFiles.map((item, idx) => (
+              <Grid item sm={3.8} xs={12} className="gridPageBox">
+                <video
+                  src={`${BASE_URL}${item.fileData[0]}`}
+                  alt={idx}
+                  id={idx}
+                  controls={false}
+                  autoPlay={true}
+                  loop={true}
+                  muted
+                  height="100%"
+                  width="100%"
+                />
+              </Grid>
+            ))
+          ) : (
+            <CircularProgress className="animationLoader" disableShrink />
+          )}s
         </Grid>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default MiddleNav
+export default MiddleNav;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,29 @@
-import { Grid } from '@mui/material'
-import React from 'react'
-import LandingNavbar from '../../components/LandingNavbar/LandingNavbar'
-import MiddleNav from '../../components/MiddleNav/MiddleNav'
-import "./Home.css"
-import backGroundVideo from "../../assets/No-Black-Start-Mobile-test.mp4"
+import { Grid } from "@mui/material";
+import React from "react";
+import LandingNavbar from "../../components/LandingNavbar/LandingNavbar";
+import MiddleNav from "../../components/MiddleNav/MiddleNav";
+import "./Home.css";
+import backGroundVideo from "../../assets/No-Black-Start-Mobile-test.mp4";
 
 const Home = () => {
   return (
     <>
       <LandingNavbar />
-      <Grid container className="flex heroDiv" >
-        <video width="100%" height="auto" controls={false} autoPlay={true} loop="true" muted >
+      <Grid container className="flex heroDiv">
+        <video
+          width="100%"
+          height="auto"
+          controls={false}
+          autoPlay={true}
+          loop={true}
+          muted
+        >
           <source src={backGroundVideo} type="video/mp4" />
         </video>
       </Grid>
       <MiddleNav />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
